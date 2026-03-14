5bc79aed8f7f7f948d7341aacf93ec69bc78b2a4
goswamimoksh455-max <goswamimoksh455@gmail.com>
2026-03-14 22:26:34 +0530

    https solved

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
-  plugins: [react()],
   server: {
     proxy: {
-      '/api': { target: 'http://localhost:3000', changeOrigin: true },
+      '/api': {
+        target: 'https://51.21.161.160',
+        changeOrigin: true,
+        secure: false, // This is the magic line that ignores the weak certificate
+      },
     },
   },
-})
+});
